fb6d660e206051a900de6aaa936ba43a1b73686b
zoonong <zoonongzung@naver.com>
2022-05-23 23:36:09 +0900

    setting 오타 수정

--- modubahchan_b/modubahchan_b/settings.py
+++ modubahchan_b/modubahchan_b/settings.py
@@ -101,7 +101,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     #react
-    'coresheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
